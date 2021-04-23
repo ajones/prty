982b5a59e5eb9058c41adf066092b0a1247dbef3
Aaron Jones <r.aaron.jones@gmail.com>
2021-04-22 21:55:22 -0700

    ver bump

--- config/config.go
+++ config/config.go
@@ -10,7 +10,7 @@ import (
 	"gopkg.in/yaml.v3"
 )
 
-const PRTYVersion = "0.0.3"
+const PRTYVersion = "0.0.4"
 const AppCacheDirName = ".prty"
 const ConfFileName = "conf.yaml"
 const StatsFileName = "stats.json"
